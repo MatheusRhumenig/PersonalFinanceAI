f11aea02dc1926361b79e533f3bf13d290ec3c26
MatheusRhumenig <mrfmatheus4@gmail.com>
2026-08-14 22:20:20 -0300

    docs: configuracao do swagger/openapi e documentacao dos controllers e dtos

--- src/main/java/com/matheusfonseca/personal_financial_ai/controller/CategoryController.java
+++ src/main/java/com/matheusfonseca/personal_financial_ai/controller/CategoryController.java
@@ -11,9 +11,13 @@ import com.matheusfonseca.personal_financial_ai.dto.CategoryRequestDTO;
 import com.matheusfonseca.personal_financial_ai.dto.CategoryResponseDTO;
 import com.matheusfonseca.personal_financial_ai.service.CategoryService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Category Controller", description = "Endpoints para gerenciamento de categorias de transações financeiras")
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
@@ -21,12 +25,18 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "Cria uma nova categoria", description = "Endpoint para criar uma nova categoria de transação financeira")
+    @ApiResponse(responseCode = "201", description = "Categoria criada com sucesso")
+    @ApiResponse(responseCode = "409", description = "Categoria já existe")
     @PostMapping("/api/category")
     public ResponseEntity<String> createCategory (@Valid @RequestBody CategoryRequestDTO categoryRequestDTO) {
         categoryService.createCategory(categoryRequestDTO);
         return ResponseEntity.status(201).body("Categoria criada com sucesso");
     }
     
+    @Operation(summary = "Obtém uma categoria pelo ID", description = "Endpoint para obter os detalhes de uma categoria específica pelo seu ID")
+    @ApiResponse(responseCode = "200", description = "Categoria encontrada com sucesso")
+    @ApiResponse(responseCode = "404", description = "Categoria não encontrada")
     @GetMapping("/api/categories/{id}")
     public ResponseEntity<CategoryResponseDTO> getCategoryById(@PathVariable Long id) {
         CategoryResponseDTO categoryResponseDTO = categoryService.getCategoryById(id);
--- src/main/java/com/matheusfonseca/personal_financial_ai/dto/TransactionRequestDTO.java
+++ src/main/java/com/matheusfonseca/personal_financial_ai/dto/TransactionRequestDTO.java
@@ -7,32 +7,40 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.matheusfonseca.personal_financial_ai.entity.enums.TransactionType;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PastOrPresent;
 
 // DTO (Data Transfer Object) para representar os dados de uma transação recebidos em uma requisição HTTP
+@Schema(description = "DTO para requisição da criação de Transactions")
 public record TransactionRequestDTO(
 
+    @Schema(description = "ID do usuário proprietário da transação", example = "2")
     @NotNull(message = "Selecione um usuário")
     Long userId,
 
+    @Schema(description = "ID da categoria associada à transação", example = "1")
     @NotNull(message = "Selecione uma categoria")
     Long categoryId,
     
+    @Schema(description = "Descrição detalhada da transação", example = "Compras da semana no Supermercado")
     @NotBlank(message = "Descrição não pode ser vazia")
     String description,
 
+    @Schema(description = "Tipo de movimentação: INCOME (Receita) ou EXPENSE (Despesa)", example = "EXPENSE")
     @NotNull(message = "Tipo de transação não pode ser vazio")
     TransactionType type,
 
     // Validação para garantir que o valor da transação seja maior que zero
+    @Schema(description = "Valor monetário da transação (deve ser maior que zero)", example = "250.75")
     @NotNull(message = "Valor não pode ser vazio")
     @DecimalMin(value = "0.01", message = "Valor deve ser maior que zero")
     BigDecimal amount,
 
     // Validação para garantir que a data da transação não seja futura e esteja no formato correto
+    @Schema(description = "Data em que a transação foi realizada (formato yyy-MM-dd)", example = "2026-08-14")
     @NotBlank(message = "Data não pode ser vazia")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @PastOrPresent(message = "Data não pode ser futura")
